be40a4d32c274b6957948187bb6e80b854f22a04
roiwei <48522938+roiwei@users.noreply.github.com>
2019-05-29 19:17:24 +0300

    fixed fail page

--- app/src/main/java/com/example/android_hw3/LoserActivity.java
+++ app/src/main/java/com/example/android_hw3/LoserActivity.java
@@ -5,14 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoserActivity extends AppCompatActivity {
     private Button restart;
+    private TextView wamba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loser);
+        wamba=findViewById(R.id.TextAnswer);
+        String wambefet=String.valueOf(getIntent().getIntExtra("Answered",10));
+        wamba.setText("Correctly Answered:"+wambefet);
         restart=findViewById(R.id.ButtonReStart);
+
         restart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -20,7 +26,7 @@ public class LoserActivity extends AppCompatActivity {
             }
         });
     }
-    
+
     public void openTriviaActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         // intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY); // Adds the FLAG_ACTIVITY_NO_HISTORY flag

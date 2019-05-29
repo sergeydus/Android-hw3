22ee0beb0e79f0bf69a9926633b494a542b9698c
roiwei <48522938+roiwei@users.noreply.github.com>
2019-05-29 18:35:41 +0300

    Update BonusActivity.java

--- app/src/main/java/com/example/android_hw3/BonusActivity.java
+++ app/src/main/java/com/example/android_hw3/BonusActivity.java
@@ -1,8 +1,15 @@
 package com.example.android_hw3;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class BonusActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bonus);
+    }
 
 //
 }
